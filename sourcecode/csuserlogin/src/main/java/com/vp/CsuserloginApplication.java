2ce8fd58263a2e9a1f38be9a9b7b414409c2bda8
susmitha2916 <srisusmitha.v@gmail.com>
2020-05-04 15:18:37 +0530

    added loginservice,zuul,jobdetail service

---
+++ sourcecode/csuserlogin/src/main/java/com/vp/CsuserloginApplication.java
@@ -0,0 +1,23 @@
+package com.vp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+@EnableEurekaClient
+@SpringBootApplication
+@EnableSwagger2
+public class CsuserloginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsuserloginApplication.class, args);
+	}
+	
+
+}
